d942f761eab70c8ada1e708a65d319e42743683c
Evren Ceyhan <evren@ceyhan.io>
2022-04-28 00:10:40 +0300

    fetching response refactored

--- src/context/ResultContextProvider.js
+++ src/context/ResultContextProvider.js
@@ -4,30 +4,27 @@ const ResultContext = createContext();
 
 const apiUrl = 'https://google-search3.p.rapidapi.com/api/v1';
 const apiKey = '65b9727b4cmshb9aaaffc5e204e3p16c0a1jsn32d97d4f188e';
-const apiHeaders = {
-    'X-User-Agent': 'desktop',
-    'X-Proxy-Location': 'EU',
-    'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
-    'X-RapidAPI-Key': apiKey,
-};
 
 export const ResultContextProvider = ({ children }) => {
     const [results, setResults] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [searchTerm, setSearchTerm] = useState('');
+    const [searchTerm, setSearchTerm] = useState('js mastery');
 
     // type: 'search' | 'images' | 'news' | 'videos'
     const fetchResults = async (query, type = 'search') => {
         setLoading(true);
 
-        const response = await fetch(`${apiUrl}/${type}?q=${query}`, {
+        const response = await fetch(`${apiUrl}/${type}/q=${query}`, {
             method: 'GET',
-            headers: apiHeaders,
+            headers: {
+                'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+                'X-RapidAPI-Key': apiKey,
+            },
         });
 
         const data = await response.json();
 
-        setResults(data.results);
+        setResults(type === 'image' ? data.image_results :  data.results);
         setLoading(false);
     };
 
