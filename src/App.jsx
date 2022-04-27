5212ffd505c568341f069ad9741994dfa6959214
Evren Ceyhan <evren@ceyhan.io>
2022-04-27 20:30:54 +0300

    dark mode impl.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
-const App = () => {
-    return <div>App</div>;
-};
+import { useState } from 'react';
+import { Navbar } from './components/Navbar';
+import { Routes } from './components/Routes';
+import { Footer } from './components/Footer';
 
-export default App;
+export const App = () => {
+    const [darkTheme, setDarkTheme] = useState(false);
+
+    return (
+        <div className={darkTheme ? 'dark' : ''}>
+            <div className="bg-gray-100 dark:bg-gray-200 dark:text-gray-900 min-h-screen">
+                <Navbar />
+                <Routes />
+                <Footer />
+            </div>
+        </div>
+    );
+};
