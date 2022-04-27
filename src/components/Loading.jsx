cdc3d8f23b2e31576a57209d2a35840fae468389
Evren Ceyhan <evren@ceyhan.io>
2022-04-27 23:09:34 +0300

    loading spinner impl.

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import {Puff} from 'react-loader-spinner';
+
+export const Loading = () => {
+    return (
+        <div className="flex justify-center items-center">
+            <Puff type="Puff" color="#00BFFF" height={550} width={80} />
+        </div>
+    );
+};
