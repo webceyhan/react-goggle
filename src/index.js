ee388e4ca33fd7d957cd7a88c11a6bc551908928
Evren Ceyhan <evren@ceyhan.io>
2022-04-27 22:53:12 +0300

    result context provider added into index

--- src/index.js
+++ src/index.js
@@ -4,25 +4,32 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './index.css';
 import { App } from './App';
 import { Results } from './components/Results';
+import { ResultContextProvider } from './context/ResultContextProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                {/* parent route (as layout) */}
-                <Route path="/" element={<App />}>
-                    {/* child routes */}
-                    {['/search', '/images', '/news', '/videos'].map((path) => (
-                        <Route path={path} element={<Results />} key={path} />
-                    ))}
+        <ResultContextProvider>
+            <BrowserRouter>
+                <Routes>
+                    {/* parent route (as layout) */}
+                    <Route path="/" element={<App />}>
+                        {/* child routes */}
+                        {['/search', '/images', '/news', '/videos'].map(
+                            (path) => (
+                                <Route
+                                    path={path}
+                                    element={<Results />}
+                                    key={path}
+                                />
+                            )
+                        )}
 
-                    {/* default redirection from home */}
-                    <Route index element={<Navigate to="/search" />} />
-                </Route>
-            </Routes>
-
-            <App />
-        </BrowserRouter>
+                        {/* default redirection from home */}
+                        <Route index element={<Navigate to="/search" />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        </ResultContextProvider>
     </React.StrictMode>
 );
