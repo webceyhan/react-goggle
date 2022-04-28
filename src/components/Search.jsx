5276db9d26baa4241e5b0a41e4b6ea434bde5c83
Evren Ceyhan <evren@ceyhan.io>
2022-04-28 18:53:22 +0300

    search styles updated

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -17,17 +17,17 @@ export const Search = () => {
             <input
                 type="text"
                 value={term}
-                className="sm:w-96 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-6 text-black hover:shadow-lg"
+                className="sm:w-96 h-10 dark:bg-gray-200 border rounded-full shadow-sm outline-none p-5 text-black hover:shadow-lg"
                 placeholder="search Goggle..."
                 onChange={(e) => setTerm(e.target.value)}
             />
             {term.length > 0 && (
                 <button
                     type="button"
-                    className="absolute top-1.5 right-4 text-2xl text-gray-500"
+                    className="absolute top-1 right-5 text-2xl text-gray-500"
                     onClick={() => setTerm('')}
                 >
-                    X
+                    x
                 </button>
             )}
 
