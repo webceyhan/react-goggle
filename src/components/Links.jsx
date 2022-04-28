52134fc8d307aba297f6578c16c483988acdd2a5
Evren Ceyhan <evren@ceyhan.io>
2022-04-28 19:46:15 +0300

    nav links restyled

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -9,14 +9,14 @@ const links = [
 
 export const Links = () => {
     return (
-        <div className="flex sm:justify-around justify-between items-center mt-4">
+        <div className="flex sm:justify-around justify-between items-center mt-3">
             {links.map(({ url, text }, index) => (
                 <NavLink
                     key={index}
                     to={url}
                     className={({ isActive }) => {
                         const classes =
-                            ' dark:text-blue-300 border-b-2 hover:border-blue-700 pb-2 m-2 mb-0';
+                            ' dark:text-blue-300 border-b-2 hover:border-blue-700 p-2 mx-1';
                         return isActive
                             ? `${classes} border-blue-700`
                             : classes;
