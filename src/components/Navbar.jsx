94526ee850f0437077a0f4a7851bcfe71a062181
Evren Ceyhan <evren@ceyhan.io>
2022-04-28 19:46:25 +0300

    navbar restyled

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,16 @@ import { Search } from './Search';
 
 export const Navbar = ({ darkTheme, setDarkTheme }) => {
     return (
-        <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200">
-            <div className="flex justify-between items-start space-x-5 w-screen">
+        <div className="flex flex-wrap sm:justify-between justify-center items-center border-b dark:border-gray-700 border-gray-200 p-5 pb-0">
+            <div className="flex justify-between items-center space-x-5 w-screen">
                 <Link to="/">
-                    <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
+                    <p className="text-2xl font-bold text-white bg-blue-500 dark:bg-slate-600 rounded py-2 px-3">
                         Goggle 🔍
                     </p>
                 </Link>
+
                 <Search />
+
                 <button
                     type="button"
                     onClick={() => setDarkTheme(!darkTheme)}
