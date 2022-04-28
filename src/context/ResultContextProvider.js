af5900a3e46904fa201d0adfd09dae1e09df26e7
Evren Ceyhan <evren@ceyhan.io>
2022-04-28 21:53:27 +0300

    sample data impl.

--- src/context/ResultContextProvider.js
+++ src/context/ResultContextProvider.js
@@ -1,9 +1,11 @@
 import { createContext, useContext, useState } from 'react';
+import { responses } from './sample';
 
 const ResultContext = createContext();
 
+const DEV = process.env.NODE_ENV === 'development';
 const apiUrl = 'https://google-search3.p.rapidapi.com/api/v1';
-const apiKey = '65b9727b4cmshb9aaaffc5e204e3p16c0a1jsn32d97d4f188e';
+const apiKey = 'c6475d3a99msh3841d49343627f9p1b1dfbjsn091c7fa8140b';
 
 const responseTypeMap = {
     search: 'results',
@@ -24,18 +26,24 @@ export const ResultContextProvider = ({ children }) => {
 
         setLoading(true);
 
-        const response = await fetch(
-            `${apiUrl}/${type}/q=${encodeURIComponent(query)}`,
-            {
-                method: 'GET',
-                headers: {
-                    'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
-                    'X-RapidAPI-Key': apiKey,
-                },
-            }
-        );
+        let data = {};
 
-        const data = await response.json();
+        if (DEV) {
+            data = responses[type];
+        } else {
+            const response = await fetch(
+                `${apiUrl}/${type}/q=${encodeURIComponent(query)}`,
+                {
+                    method: 'GET',
+                    headers: {
+                        'X-RapidAPI-Host': 'google-search3.p.rapidapi.com',
+                        'X-RapidAPI-Key': apiKey,
+                    },
+                }
+            );
+
+            data = await response.json();
+        }
 
         setResults(data[responseTypeMap[type]] ?? []);
         setLoading(false);
---
+++ src/context/sample.js
@@ -0,0 +1,314 @@
+const searchResponse = {
+    results: [
+        {
+            description:
+                'Signature. Elon Reeve Musk FRS (/ˈiːlɒn/; born June 28, 1971) is an engineer, industrial designer, technology entrepreneur and philanthropist.',
+            link: 'https://en.wikipedia.org/wiki/Elon_Musk',
+            title: 'Elon Musk - Wikipedia',
+        },
+        {
+            description:
+                'Elon Musk is working to revolutionize transportation both on Earth, through electric car maker Tesla - and in space, via rocket producer SpaceX. He owns 21% of ...',
+            link: 'https://www.forbes.com/profile/elon-musk/',
+            title: 'Elon Musk - Forbes',
+        },
+        {
+            description:
+                'May 26, 2020 - In 1992, Musk left Canada to study business and physics at the University of Pennsylvania. He graduated with an undergraduate degree in ...',
+            link: 'https://www.biography.com/business-figure/elon-musk',
+            title: 'Elon Musk - Education, Tesla & SpaceX - Biography',
+        },
+        {
+            description:
+                'Jun 24, 2020 - Elon Musk, South African-born American entrepreneur who cofounded the electronic-payment firm PayPal and formed SpaceX, maker of ...',
+            link: 'https://www.britannica.com/biography/Elon-Musk',
+            title: 'Elon Musk | Biography & Facts | Britannica',
+        },
+        {
+            description:
+                "Jul 13, 2020 - Elon Musk's net worth just hit $70.5 billion, surpassing Warren Buffett's. Here's how the billionaire Tesla and SpaceX CEO went from getting ...",
+            link: 'https://www.businessinsider.com/the-rise-of-elon-musk-2016-7',
+            title: "Elon Musk's life story: The Tesla CEO's early years, career ...",
+        },
+        {
+            description:
+                'Signature. Elon Reeve Musk FRS (/ˈiːlɒn/; born June 28, 1971) is an engineer, industrial designer, technology entrepreneur and philanthropist.',
+            link: 'https://en.wikipedia.org/wiki/Elon_Musk',
+            title: 'Elon Musk - Wikipedia',
+        },
+        {
+            description:
+                'Elon Musk is working to revolutionize transportation both on Earth, through electric car maker Tesla - and in space, via rocket producer SpaceX. He owns 21% of ...',
+            link: 'https://www.forbes.com/profile/elon-musk/',
+            title: 'Elon Musk - Forbes',
+        },
+        {
+            description:
+                'May 26, 2020 - In 1992, Musk left Canada to study business and physics at the University of Pennsylvania. He graduated with an undergraduate degree in ...',
+            link: 'https://www.biography.com/business-figure/elon-musk',
+            title: 'Elon Musk - Education, Tesla & SpaceX - Biography',
+        },
+        {
+            description:
+                'Jun 24, 2020 - Elon Musk, South African-born American entrepreneur who cofounded the electronic-payment firm PayPal and formed SpaceX, maker of ...',
+            link: 'https://www.britannica.com/biography/Elon-Musk',
+            title: 'Elon Musk | Biography & Facts | Britannica',
+        },
+        {
+            description:
+                "Jul 13, 2020 - Elon Musk's net worth just hit $70.5 billion, surpassing Warren Buffett's. Here's how the billionaire Tesla and SpaceX CEO went from getting ...",
+            link: 'https://www.businessinsider.com/the-rise-of-elon-musk-2016-7',
+            title: "Elon Musk's life story: The Tesla CEO's early years, career ...",
+        },
+    ],
+};
+
+const newsResponse = {
+    entries: [
+        {
+            guidislink: false,
+            id: 'CBMiaWh0dHBzOi8vd3d3LndoaXRlaG91c2UuZ292L2JyaWVmaW5ncy1zdGF0ZW1lbnRzL3ByZXNzLWJyaWVmaW5nLXByZXNzLXNlY3JldGFyeS1rYXlsZWlnaC1tY2VuYW55LTctOS0yMDIwL9IBAA',
+            link: 'https://www.whitehouse.gov/briefings-statements/press-briefing-press-secretary-kayleigh-mcenany-7-9-2020/',
+            published: 'Thu, 09 Jul 2020 22:18:58 GMT',
+            title: 'Press Briefing by Press Secretary Kayleigh McEnany | 7/9/2020 - Whitehouse.gov',
+            source: {
+                href: 'https://www.whitehouse.gov',
+                title: 'Whitehouse.gov',
+            },
+        },
+        {
+            guidislink: false,
+            id: 'CBMiZGh0dHBzOi8vd3d3LndoaXRlaG91c2UuZ292L2JyaWVmaW5ncy1zdGF0ZW1lbnRzL3JlbWFya3MtcHJlc2lkZW50LXRydW1wLXJvdW5kdGFibGUtaGlzcGFuaWMtbGVhZGVycy_SAQA',
+            link: 'https://www.whitehouse.gov/briefings-statements/remarks-president-trump-roundtable-hispanic-leaders/',
+            published: 'Thu, 09 Jul 2020 22:18:58 GMT',
+            title: 'Remarks by President Trump During Roundtable with Hispanic Leaders - Whitehouse.gov',
+            source: {
+                href: 'https://www.whitehouse.gov',
+                title: 'Whitehouse.gov',
+            },
+        },
+        {
+            guidislink: false,
+            id: 'CBMiZ2h0dHBzOi8vd3d3LndoaXRlaG91c2UuZ292L2JyaWVmaW5ncy1zdGF0ZW1lbnRzL3ByZXNzLWJyaWVmaW5nLXByZXNzLXNlY3JldGFyeS1rYXlsZWlnaC1tY2VuYW55LTA3MDYyMC_SAQA',
+            link: 'https://www.whitehouse.gov/briefings-statements/press-briefing-press-secretary-kayleigh-mcenany-070620/',
+            published: 'Mon, 06 Jul 2020 20:50:10 GMT',
+            title: 'Press Briefing by Press Secretary Kayleigh McEnany - Whitehouse.gov',
+            source: {
+                href: 'https://www.whitehouse.gov',
+                title: 'Whitehouse.gov',
+            },
+        },
+        {
+            guidislink: false,
+            id: 'CBMiaGh0dHBzOi8vd3d3LndoaXRlaG91c2UuZ292L2JyaWVmaW5ncy1zdGF0ZW1lbnRzL3JlbWFya3MtcHJlc2lkZW50LXRydW1wLWZveC1uZXdzLXRvd24taGFsbC1ncmVlbi1iYXktd2kv0gEA',
+            link: 'https://www.whitehouse.gov/briefings-statements/remarks-president-trump-fox-news-town-hall-green-bay-wi/',
+            published: 'Thu, 25 Jun 2020 07:00:00 GMT',
+            title: 'Remarks by President Trump in a Fox News Town Hall | Green Bay, WI - Whitehouse.gov',
+            source: {
+                href: 'https://www.whitehouse.gov',
+                title: 'Whitehouse.gov',
+            },
+        },
+        {
+            guidislink: false,
+            id: 'CBMiemh0dHBzOi8vdGhlY29udmVyc2F0aW9uLmNvbS9xYW5vbi1jb25zcGlyYWN5LXRoZW9yeS1mb2xsb3dlcnMtc3RlcC1vdXQtb2YtdGhlLXNoYWRvd3MtYW5kLW1heS1iZS1oZWFkZWQtdG8tY29uZ3Jlc3MtMTQxNTgx0gF-aHR0cHM6Ly90aGVjb252ZXJzYXRpb24uY29tL2FtcC9xYW5vbi1jb25zcGlyYWN5LXRoZW9yeS1mb2xsb3dlcnMtc3RlcC1vdXQtb2YtdGhlLXNoYWRvd3MtYW5kLW1heS1iZS1oZWFkZWQtdG8tY29uZ3Jlc3MtMTQxNTgx',
+            link: 'https://theconversation.com/qanon-conspiracy-theory-followers-step-out-of-the-shadows-and-may-be-headed-to-congress-141581',
+            published: 'Wed, 08 Jul 2020 16:55:00 GMT',
+            title: 'QAnon conspiracy theory followers step out of the shadows and may be headed to Congress - The Conversation CA',
+            source: {
+                href: 'https://theconversation.com',
+                title: 'The Conversation CA',
+            },
+        },
+
+        {
+            guidislink: false,
+            id: 'CBMiaWh0dHBzOi8vd3d3LndoaXRlaG91c2UuZ292L2JyaWVmaW5ncy1zdGF0ZW1lbnRzL3ByZXNzLWJyaWVmaW5nLXByZXNzLXNlY3JldGFyeS1rYXlsZWlnaC1tY2VuYW55LTctOS0yMDIwL9IBAA',
+            link: 'https://www.whitehouse.gov/briefings-statements/press-briefing-press-secretary-kayleigh-mcenany-7-9-2020/',
+            published: 'Thu, 09 Jul 2020 22:18:58 GMT',
+            title: 'Press Briefing by Press Secretary Kayleigh McEnany | 7/9/2020 - Whitehouse.gov',
+            source: {
+                href: 'https://www.whitehouse.gov',
+                title: 'Whitehouse.gov',
+            },
+        },
+        {
+            guidislink: false,
+            id: 'CBMiZGh0dHBzOi8vd3d3LndoaXRlaG91c2UuZ292L2JyaWVmaW5ncy1zdGF0ZW1lbnRzL3JlbWFya3MtcHJlc2lkZW50LXRydW1wLXJvdW5kdGFibGUtaGlzcGFuaWMtbGVhZGVycy_SAQA',
+            link: 'https://www.whitehouse.gov/briefings-statements/remarks-president-trump-roundtable-hispanic-leaders/',
+            published: 'Thu, 09 Jul 2020 22:18:58 GMT',
+            title: 'Remarks by President Trump During Roundtable with Hispanic Leaders - Whitehouse.gov',
+            source: {
+                href: 'https://www.whitehouse.gov',
+                title: 'Whitehouse.gov',
+            },
+        },
+        {
+            guidislink: false,
+            id: 'CBMiZ2h0dHBzOi8vd3d3LndoaXRlaG91c2UuZ292L2JyaWVmaW5ncy1zdGF0ZW1lbnRzL3ByZXNzLWJyaWVmaW5nLXByZXNzLXNlY3JldGFyeS1rYXlsZWlnaC1tY2VuYW55LTA3MDYyMC_SAQA',
+            link: 'https://www.whitehouse.gov/briefings-statements/press-briefing-press-secretary-kayleigh-mcenany-070620/',
+            published: 'Mon, 06 Jul 2020 20:50:10 GMT',
+            title: 'Press Briefing by Press Secretary Kayleigh McEnany - Whitehouse.gov',
+            source: {
+                href: 'https://www.whitehouse.gov',
+                title: 'Whitehouse.gov',
+            },
+        },
+        {
+            guidislink: false,
+            id: 'CBMiaGh0dHBzOi8vd3d3LndoaXRlaG91c2UuZ292L2JyaWVmaW5ncy1zdGF0ZW1lbnRzL3JlbWFya3MtcHJlc2lkZW50LXRydW1wLWZveC1uZXdzLXRvd24taGFsbC1ncmVlbi1iYXktd2kv0gEA',
+            link: 'https://www.whitehouse.gov/briefings-statements/remarks-president-trump-fox-news-town-hall-green-bay-wi/',
+            published: 'Thu, 25 Jun 2020 07:00:00 GMT',
+            title: 'Remarks by President Trump in a Fox News Town Hall | Green Bay, WI - Whitehouse.gov',
+            source: {
+                href: 'https://www.whitehouse.gov',
+                title: 'Whitehouse.gov',
+            },
+        },
+        {
+            guidislink: false,
+            id: 'CBMiemh0dHBzOi8vdGhlY29udmVyc2F0aW9uLmNvbS9xYW5vbi1jb25zcGlyYWN5LXRoZW9yeS1mb2xsb3dlcnMtc3RlcC1vdXQtb2YtdGhlLXNoYWRvd3MtYW5kLW1heS1iZS1oZWFkZWQtdG8tY29uZ3Jlc3MtMTQxNTgx0gF-aHR0cHM6Ly90aGVjb252ZXJzYXRpb24uY29tL2FtcC9xYW5vbi1jb25zcGlyYWN5LXRoZW9yeS1mb2xsb3dlcnMtc3RlcC1vdXQtb2YtdGhlLXNoYWRvd3MtYW5kLW1heS1iZS1oZWFkZWQtdG8tY29uZ3Jlc3MtMTQxNTgx',
+            link: 'https://theconversation.com/qanon-conspiracy-theory-followers-step-out-of-the-shadows-and-may-be-headed-to-congress-141581',
+            published: 'Wed, 08 Jul 2020 16:55:00 GMT',
+            title: 'QAnon conspiracy theory followers step out of the shadows and may be headed to Congress - The Conversation CA',
+            source: {
+                href: 'https://theconversation.com',
+                title: 'The Conversation CA',
+            },
+        },
+    ],
+};
+
+const imageResponse = {
+    image_results: [
+        {
+            image: {
+                alt: 'Tesla Model S — Wikipédia',
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTcyfRzUGtO3mSZAAaAG-SbsljU7esjzmT5lQ&usqp=CAU',
+            },
+            link: {
+                domain: 'fr.wikipedia.org',
+                href: 'https://fr.wikipedia.org/wiki/Tesla_Model_S',
+                title: 'Tesla Model S — Wikipédia',
+            },
+        },
+
+        {
+            image: {
+                alt: 'Tesla Model S hits milestone in Norway with 20k total registrations',
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTwhfgafGneYwzBKaSEzGsS-ugj-3WHDGmAig&usqp=CAU',
+            },
+            link: {
+                domain: 'teslarati.com',
+                href: 'https://www.teslarati.com/tesla-model-s-20k-milestone-norway/',
+                title: 'Tesla Model S hits milestone in Norway ...',
+            },
+        },
+        {
+            image: {
+                alt: "Tesla's Self-Driving Software Gets a Major Update | WIRED",
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRmsxnwoceLA1hl_u0MgMbBa35fXTAoumf1JA&usqp=CAU',
+            },
+            link: {
+                domain: 'wired.com',
+                href: 'https://www.wired.com/2016/09/teslas-self-driving-software-gets-major-update/',
+                title: "Tesla's Self-Driving Software Gets a ...",
+            },
+        },
+        {
+            image: {
+                alt: 'https://img.autoplus.fr/news/2020/06/18/1548918/165cb604af0026309f2df0b6-1200-800.jpg',
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQiqv9uCCFzJCZ57BV4-dld8tQ5Fl2skSKeWg&usqp=CAU',
+            },
+            link: {
+                domain: 'autoplus.fr',
+                href: 'https://www.autoplus.fr/tesla-motors/model-y/actualite/Tesla-Model-Y-Elon-Musk-electrique-defauts-1548918.html',
+                title: 'Les acquéreurs du Tesla Model Y se ...',
+            },
+        },
+        {
+            image: {
+                alt: 'https://img.phonandroid.com/2020/05/tesla-model-s-long-range.jpg',
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS-ei6hbubjVFklSTj9QPcdu-HdYLr9GV9d0w&usqp=CAU',
+            },
+            link: {
+                domain: 'phonandroid.com',
+                href: 'https://www.phonandroid.com/tesla-model-s-elon-musk-baratine-sur-lautonomie-reelle-du-modele-long-range.html',
+                title: 'Tesla Model S : Elon Musk a menti sur l ...',
+            },
+        },
+        {
+            image: {
+                alt: 'Tesla Model S — Wikipédia',
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTcyfRzUGtO3mSZAAaAG-SbsljU7esjzmT5lQ&usqp=CAU',
+            },
+            link: {
+                domain: 'fr.wikipedia.org',
+                href: 'https://fr.wikipedia.org/wiki/Tesla_Model_S',
+                title: 'Tesla Model S — Wikipédia',
+            },
+        },
+
+        {
+            image: {
+                alt: 'Tesla Model S hits milestone in Norway with 20k total registrations',
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTwhfgafGneYwzBKaSEzGsS-ugj-3WHDGmAig&usqp=CAU',
+            },
+            link: {
+                domain: 'teslarati.com',
+                href: 'https://www.teslarati.com/tesla-model-s-20k-milestone-norway/',
+                title: 'Tesla Model S hits milestone in Norway ...',
+            },
+        },
+        {
+            image: {
+                alt: "Tesla's Self-Driving Software Gets a Major Update | WIRED",
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRmsxnwoceLA1hl_u0MgMbBa35fXTAoumf1JA&usqp=CAU',
+            },
+            link: {
+                domain: 'wired.com',
+                href: 'https://www.wired.com/2016/09/teslas-self-driving-software-gets-major-update/',
+                title: "Tesla's Self-Driving Software Gets a ...",
+            },
+        },
+        {
+            image: {
+                alt: 'https://img.autoplus.fr/news/2020/06/18/1548918/165cb604af0026309f2df0b6-1200-800.jpg',
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQiqv9uCCFzJCZ57BV4-dld8tQ5Fl2skSKeWg&usqp=CAU',
+            },
+            link: {
+                domain: 'autoplus.fr',
+                href: 'https://www.autoplus.fr/tesla-motors/model-y/actualite/Tesla-Model-Y-Elon-Musk-electrique-defauts-1548918.html',
+                title: 'Les acquéreurs du Tesla Model Y se ...',
+            },
+        },
+        {
+            image: {
+                alt: 'https://img.phonandroid.com/2020/05/tesla-model-s-long-range.jpg',
+                src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS-ei6hbubjVFklSTj9QPcdu-HdYLr9GV9d0w&usqp=CAU',
+            },
+            link: {
+                domain: 'phonandroid.com',
+                href: 'https://www.phonandroid.com/tesla-model-s-elon-musk-baratine-sur-lautonomie-reelle-du-modele-long-range.html',
+                title: 'Tesla Model S : Elon Musk a menti sur l ...',
+            },
+        },
+    ],
+};
+
+const videoResponse = {
+    results: [
+        { link: 'https://www.youtube.com/watch?v=I5XKk17GksY' },
+        { link: 'https://www.youtube.com/watch?v=vO7FTmEzsnE' },
+        { link: 'https://www.youtube.com/watch?v=jY7LS0YmwcI' },
+        { link: 'https://www.youtube.com/watch?v=RK0qdmkhcoQ' },
+        { link: 'https://www.youtube.com/watch?v=RiTVcFJef5Q' },
+    ],
+};
+
+export const responses = {
+    search: searchResponse,
+    image: imageResponse,
+    video: videoResponse,
+    news: newsResponse,
+};
