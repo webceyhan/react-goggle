750ca34b2bb7e7c8387c22c687143e51a7117b41
Evren Ceyhan <evren@ceyhan.io>
2022-04-28 22:31:47 +0300

    bugfix: object are not valid as a React child...

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -18,14 +18,21 @@ export const Results = () => {
         case '/search':
             return (
                 <div className="flex flex-wrap justify-center space-y-5 sm:px-5">
-                    {results?.map(({ link, title }, index) => (
+                    {results.map(({ link, title }, index) => (
                         <div className="md:w-2/3 w-full" key={index}>
                             <a href={link} target="_blank" rel="noreferrer">
+                                {/* 
+                                    bugfix: 
+                                    react-dom.development.js:14757 Uncaught Error: Objects are not valid 
+                                    as a React child (found: object with keys {domain, href, title}). 
+                                    If you meant to render a collection of children, use an array instead.
+
+                                    instead of {link} we use {`${link}`}
+                                */}
                                 <p className="text-sm">
-                                    {/* truncate if text is too long */}
                                     {link.length > 30
-                                        ? link.substring(0, 30) + '...'
-                                        : link}
+                                        ? `${link.substr(0, 30)}...`
+                                        : `${link}`}
                                 </p>
 
                                 <p className="text-lg hover:underline dark:text-blue-300 text-blue-700">
@@ -40,7 +47,7 @@ export const Results = () => {
         case '/image':
             return (
                 <div className="flex flex-wrap justify-center items-start">
-                    {results?.map(({ image, link: { href, title } }, index) => (
+                    {results.map(({ image, link: { href, title } }, index) => (
                         <a
                             className="sm:p-3 p-5"
                             href={href}
@@ -48,7 +55,12 @@ export const Results = () => {
                             target="_blank"
                             rel="noreferrer"
                         >
-                            <img src={image?.src} alt={title} loading="lazy" className='h-36 w-56 object-cover' />
+                            <img
+                                src={image?.src}
+                                alt={title}
+                                loading="lazy"
+                                className="h-36 w-56 object-cover"
+                            />
                             <p className="w-36 break-words text-sm mt-2">
                                 {title}
                             </p>
@@ -60,7 +72,7 @@ export const Results = () => {
         case '/news':
             return (
                 <div className="flex flex-wrap justify-center space-y-5 sm:px-5">
-                    {results?.map(({ link, id, source, title }, index) => (
+                    {results.map(({ link, source, title }, index) => (
                         <div className="md:w-2/5 w-full px-5" key={index}>
                             <a
                                 href={link}
@@ -89,7 +101,7 @@ export const Results = () => {
         case '/video':
             return (
                 <div className="flex flex-wrap justify-center space-y-5 sm:px-5">
-                    {results?.map(({ link }, index) => (
+                    {results.map(({ link }, index) => (
                         <div className="p-2" key={index}>
                             <ReactPlayer
                                 url={link}
